c463eb81ef36abada8a137adf9af6ad737ca5339
VASONE Antoine <antoinevasone@outlook.com>
2022-06-12 13:40:56 +0200

    add addToCart Live#action

--- src/Components/ProductAddToCart.php
+++ src/Components/ProductAddToCart.php
@@ -2,11 +2,15 @@
 
 namespace App\Components;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use App\Entity\Cart;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[AsLiveComponent('add_cart')]
 final class ProductAddToCart extends AbstractController
@@ -14,11 +18,25 @@ final class ProductAddToCart extends AbstractController
     use DefaultActionTrait;
 
     #[LiveProp(writable: true)]
+    public int $idBase;
     public bool $isSaved = false;
 
     #[LiveAction]
-    public function save()
+    public function save(ManagerRegistry $doctrine, #[LiveArg] int $id)
     {
+        $entityManager = $doctrine->getManager();
+
+        $product = $doctrine->getRepository(Product::class)->find($id);
+
+
+
+        $cart = new Cart();
+
+        $cart->setProductId($product);
+
+        $entityManager->persist($cart);
+        $entityManager->flush();
+
         $this->isSaved = true;
     }
 }
